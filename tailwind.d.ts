a52c57c373dd18cbfcae096346b4e2dd90f37a88
lightyen <lightyen0123@gmail.com>
2021-11-09 15:43:38 +0800

    3.0.0-alpha.2.6

--- tailwind.d.ts
+++ tailwind.d.ts
@@ -1720,7 +1720,9 @@ declare namespace Tailwind {
 			options?: Record<string, any>
 		},
 		(
+			| import("postcss").Comment
 			| import("postcss").Rule
+			| import("postcss").AtRule
 			| ((
 					modifier: string,
 			  ) => Array<Record<string, Record<string, string>>>)
